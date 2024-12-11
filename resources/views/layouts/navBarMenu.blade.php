ca2f776961dbf1f7ce69ad251aa18fa7c26f30ef
taugany <tauganyc@hotmail.com>
2024-12-11 15:52:09 -0300

    Fix: adicionar botão home e logout no site

--- resources/views/layouts/navBarMenu.blade.php
+++ resources/views/layouts/navBarMenu.blade.php
@@ -1,9 +1,10 @@
 <div class="border-end" id="sidebar-wrapper">
     <div class="sidebar-heading p-3 fs-5">Menu</div>
     <div class="list-group list-group-flush">
-        <a href="#" class="list-group-item list-group-item-action">Dashboard</a>
-        <a href="#" class="list-group-item list-group-item-action">Profile</a>
-        <a href="#" class="list-group-item list-group-item-action">Settings</a>
-        <a href="#" class="list-group-item list-group-item-action">Logout</a>
+        <a href="{{route('index')}}" class="list-group-item list-group-item-action">Home</a>
+        <form method="post" action="{{route('logout')}}">
+            @csrf
+            <a type="submit" class="list-group-item list-group-item-action">Logout</a>
+        </form>
     </div>
 </div>
